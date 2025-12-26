8553648ecf7e5d81ad2008a646152c6763516883
krishna raj <krishnaraj143007@gmail.com>
2025-12-26 09:44:45 +0530

    Update CORS to allow all origins and configure frontend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+        baseURL: 'https://car-booking-xsp6.onrender.com/api',
+
 });
 
 api.interceptors.request.use((config) => {
